813f08e144fd3825b30a66398fee6fc0f00c3907
stephanieonyekaba <onyek009@umn.edu>
2022-07-18 16:11:16 -0400

    2nd prop image inserted

--- src/Home.js
+++ src/Home.js
@@ -11,9 +11,24 @@ function Home() {
        <h1 className='home_image_text'>Beauty Starts Here </h1>
 
     <div className="home__row">
-    <Product company='Fenty Beauty by Rihanna' title='Gloss Bomb Universal Lip Luminize' price= {19.99} image="https://i.ibb.co/9ZCqdHK/Screen-Shot-2022-07-18-at-1-48-35-AM.png"
+  <Product 
+    id="1"
+    company='Fenty Beauty by Rihanna'
+    title='Gloss Bomb Universal Lip Luminize' 
+    price= {19.99} 
+    image="https://i.ibb.co/9ZCqdHK/Screen-Shot-2022-07-18-at-1-48-35-AM.png"
     rating={5}/>
-    <Product />
+
+
+  <Product 
+    id="2"
+    company='NARS'
+    title='Liquid Blush' 
+    price= {39.00} 
+    image="https://i.ibb.co/pyqfbzK/Screen-Shot-2022-07-18-at-1-47-07-AM.png"
+    rating={4}/>
+
+
       {/* product */}
     </div>
     <div className="home__row">
@@ -34,5 +49,9 @@ function Home() {
 }
 
 export default Home
+
+
+
+
 
 
