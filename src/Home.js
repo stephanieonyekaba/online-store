45b3e187422b0fa3b18d049dde370884aa4a8d39
stephanieonyekaba <onyek009@umn.edu>
2022-07-21 20:26:36 -0400

    footer added

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,10 @@ import './Home.css'
 import Product from './Product'
 import Banner from './Banner'
 import Footer from './Footer'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 function Home() {
   return (
@@ -134,9 +138,70 @@ id="11"
       image="https://i.ibb.co/tZTTMP4/gradienta-gw-E9v-XSi7-Xw-unsplash.jpg "
       />
 
-<h1 className='footer_image_text'> #BRAND BEAUTY 
-       </h1>
+    <div className='footer_image_text'>
+          <div className='footer_h1'> <h1>#BRAND BEAUTY</h1> </div>
+
+    <div className="footer_text">
+    <p>
+      <strong>ABOUT</strong>
+      <li>
+        About Us
+          </li>
+          <li>
+          Brand Rewards
+          </li>
+          <li>
+          Privacy Policy
+          </li>
+    </p>
+
+    <p>
+      <strong>SUPPORT + FAQ</strong>
+          <li>
+            Contact Us
+          </li>
+          <li>
+          Shipping & Returns
+          </li>
+          <li>
+          FAQs
+          </li>
+    </p>
+    <p>
+      <strong>JOIN OUR COMMUNITY!</strong>
+          <li>
+          Receive 15% off your first order!
+          </li>
+          <li>
+            Join #Brand
+          </li>
+          <li>
+            Sign Up for Our Newsletter
+          </li>
+    </p>
+  </div>
+
+  
     </div>
+    <div className="footer_icon_text">
+            <InstagramIcon />
+            <TwitterIcon />
+            <FacebookIcon />
+            <YouTubeIcon />
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
   )
 }
 
